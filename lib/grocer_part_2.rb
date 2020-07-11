cb9388d52e46e7223dcfa5590e57100a8f5c0bc0
Ryan Werner <rmwerner@umich.edu>
2020-07-11 17:57:42 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,7 +4,7 @@ require 'pry'
 def apply_coupons(cart, coupons)
   new_cart = consolidate_cart(cart)
   coupon_items = []
-  coupon.each do |element|
+  coupons.each do |element|
     original_item = find_item_by_name_in_collection(element[:item], cart)
     coupon_item = {:item => element[:item], :price => element[:cost] / element[:num], :clearance => original_item[:clearance], :count => element[:num]}
     coupon_items << coupon_item
