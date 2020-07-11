ebee1198f791924589b6eaf70ea3c58a0f956f3f
Ryan Werner <rmwerner@umich.edu>
2020-07-11 17:46:00 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -2,23 +2,16 @@ require_relative './part_1_solution.rb'
 require 'pry'
 
 def apply_coupons(cart, coupons)
- new_cart = consolidate_cart(cart)
- new_cart.each do |item|
-   coupons.each do |coupon|
-     if item[:item] == coupon[:item] && item[:count] >= coupon[:num] && item[:price]*item[:count] != coupon[:cost]
-       discount_item = find_item_by_name_in_collection(item[:item], cart)
-       discount_item[:count] = coupon[:num]
-       discount_item[:price] = coupon[:cost] / coupon[:num]
-       cart << discount_item
-       binding.pry
-        if item[:count] > coupon[:num]
-          item[:count] = item[:count] - coupon[:num]
-        else cart - item
-        end
+  new_cart = consolidate_cart(cart)
+  new_cart.each do |item|
+    coupons.each do |coupon|
+      if item[:item] == coupon[:item]
+        string = "hello"
       end
     end
   end
 end
+
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
